289814225e4b3a888003568af203c9b5be90ce5b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-12-17 12:05:24 -0300

    env: [add] psycopg3 conneciton pooling

--- src/settings/databases.py
+++ src/settings/databases.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from sc4py.env import env
+from sc4py.env import env, env_as_int
 
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
@@ -10,7 +10,21 @@ DATABASES = {
         "NAME": env("POSTGRES_DATABASE", "painel"),
         "USER": env("POSTGRES_USER", "ava_user"),
         "PASSWORD": env("POSTGRES_PASSWORD", "ava_pass"),
-        "OPTIONS": {"options": env("POSTGRES_OPTIONS", "")},
+        "OPTIONS": {
+            "options": env("POSTGRES_OPTIONS", ""),
+            "autocommit": False,
+            "pool": {
+                "min_size": env_as_int("DEFAULT_POOL_MIN_SIZE", 1),
+                "max_size": env_as_int("DEFAULT_POOL_MAX_SIZE", 100),
+                # "open": env_as_bool("DEFAULT_POOL_OPEN", True),
+                "name": env("DEFAULT_POOL_NAME", ""),
+                "timeout": env_as_int("DEFAULT_POOL_TIMEOUT", 1),
+                "max_lifetime": env_as_int("DEFAULT_POOL_MAX_LIFETIME", 1200),
+                "max_idle": env_as_int("DEFAULT_POOL_MAX_IDLE", 600),
+                "reconnect_timeout": env_as_int("DEFAULT_POOL_RECONNECT_TIMEOUT", 300),
+                "num_workers": env_as_int("DEFAULT_POOL_NUM_WORKERS", 3),
+            }
+        },
     }
 }
 
