b953692b44ff63331a37ffa945e3b175904f63d8
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-12-20 18:48:12 -0300

    refactor: [add] melhor o log do gunicorn

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -8,5 +8,12 @@ default_web_concurrency = multiprocessing.cpu_count() * int(os.getenv("WORKER_PE
 workers = int(os.getenv("WEB_CONCURRENCY", default_web_concurrency))
 worker_class = os.getenv("WORKER_CLASS", "sync")
 worker_tmp_dir = "/dev/shm"
-loglevel = "info"
+loglevel = "debug"
 timeout = int(os.getenv("WORKER_TIMEOUT", 120))
+error_logfile = "-"
+errorlog = "-"
+accesslog = "-"
+capture_output = False
+syslog = False
+logger_class = 'gunicorn.glogging.Logger'
+
