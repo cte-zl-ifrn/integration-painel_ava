613dd2594fbb945b0d0f5b971a97486684cfd93b
HilquiasAbias <hilquias.dev@gmail.com>
2025-04-09 16:25:55 -0300

    fix: corrigindo caminho temporario

--- src/painel/urls.py
+++ src/painel/urls.py
@@ -9,8 +9,8 @@ app_name = PainelConfig.name
 
 urlpatterns = [
     path("api/v1/", api.urls),
-    path("anterior/", dashboard, name="dashboard"),
-    path("", novo, name="novo"),
+    path("", dashboard, name="dashboard"),
+    path("novo/", novo, name="novo"),
     # path("diario/<id_diario>/syncs/", syncs, name="syncs"),
     path("diario/<id_ambiente>/<id_diario>/checkgrades/", checkgrades, name="checkgrades"),
 ]
