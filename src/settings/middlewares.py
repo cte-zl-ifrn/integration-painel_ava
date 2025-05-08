7e67822db845cb6a13cd8e592347a4c9a022d560
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-05-08 16:42:03 -0300

    style: imports desnecessários

--- src/settings/middlewares.py
+++ src/settings/middlewares.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from sc4py.env import env_as_bool
-from django.core.handlers import exception
-from debug_toolbar import middleware
-
 # Middleware
 MIDDLEWARE = [
     "painel.middleware.ExceptionMiddleware",
