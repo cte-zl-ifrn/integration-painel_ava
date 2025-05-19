5e3c585e965fb2b0f1744ab9679b03875b26a9e9
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-05-19 17:18:44 -0300

    fix: problema de não carregar os estáticos

--- src/urls.py
+++ src/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 admin.site.site_title = "Painel AVA :.: Administração"
 admin.site.site_header = admin.site.site_title
@@ -27,12 +26,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += [
-        re_path(r"^static/(?P<path>\+*)$", serve, {"document_root": settings.STATIC_ROOT}),
-    ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     try:
