e4fb2140b6fa2735b4459bc9f80afd9bdc9097ff
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-05-19 17:18:12 -0300

    fix: problema de não carregar os estáticos

--- src/asgi.py
+++ src/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_asgi_application()
+application = WhiteNoise(application)
