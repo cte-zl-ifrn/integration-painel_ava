ce3647d5c42e5f9a9460e82f600d564bff7c2dae
Matheus Mathias <mathias.matheus76@gmail.com>
2026-05-27 10:06:33 -0300

    fix(urls): remover pass e adicionar log de aviso ao falhar ao carregar debug toolbar

--- src/urls.py
+++ src/urls.py
@@ -34,7 +34,6 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if DEBUG and not TESTING_MODE:
     try:
         from debug_toolbar.toolbar import debug_toolbar_urls
-
         urlpatterns += debug_toolbar_urls()
     except ImportError:
-        pass
+        logging.warning("Debug toolbar não encontrado, ignorando carregamento.")
