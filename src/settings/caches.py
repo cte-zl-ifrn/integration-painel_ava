4a576c046b2736f20231c0ae104cf6463687da80
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-06-02 21:47:22 -0300

    upd(settings): ajusta configuração de caches para suportar backend Redis ou DummyCache

--- src/settings/caches.py
+++ src/settings/caches.py
@@ -2,12 +2,19 @@
 
 from sc4py.env import env, env_as_bool, env_as_int, env_as_list
 
-CACHES = {
-    "default": {
-        "BACKEND": env("DJANGO_CACHES_DEFAULT_BACKEND", "django.core.cache.backends.redis.RedisCache"),
-        "LOCATION": env_as_list("DJANGO_CACHES_DEFAULT_LOCATION", "redis://cache:6379/1"),
+if env("DJANGO_CACHES_DEFAULT_BACKEND") == "django.core.cache.backends.redis.RedisCache":
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": env_as_list("DJANGO_CACHES_DEFAULT_LOCATION", "redis://cache:6379/1"),
+        }
     }
-}
+else:
+    CACHES = {
+        "default": {
+            "BACKEND": env("DJANGO_CACHES_DEFAULT_BACKEND", "django.core.cache.backends.dummy.DummyCache"),
+        }
+    }
 
-DASHBOARD_CACHE_ENABLED = env_as_bool("DASHBOARD_CACHE_ENABLED", True)
+DASHBOARD_CACHE_ENABLED = env_as_bool("DASHBOARD_CACHE_ENABLED", False)
 DASHBOARD_CACHE_TIMEOUT = env_as_int("DASHBOARD_CACHE_TIMEOUT", 300)
