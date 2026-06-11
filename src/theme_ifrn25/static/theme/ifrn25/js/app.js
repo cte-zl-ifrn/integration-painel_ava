02110ab5b5c466ecc3562579f27b0aae33bace3f
Matheus Mathias <mathias.matheus76@gmail.com>
2026-06-11 10:18:07 -0300

    feat(app): adicionar função getSalasIniciais para inicialização dinâmica das salas
    - Modifica loading para true durante a inicialização.

--- src/theme_ifrn25/static/theme/ifrn25/js/app.js
+++ src/theme_ifrn25/static/theme/ifrn25/js/app.js
@@ -1,5 +1,23 @@
 import * as VueSelect from './vue-select.js';
 
+const getSalasIniciais = () => {
+    const nomesAdmin = window.PAGE_CONFIG?.nomesAbas || {};
+    const salas = {};
+    const keys = Object.keys(nomesAdmin);
+    
+    if (keys.length > 0) {
+        keys.forEach(key => {
+            if (nomesAdmin[key].sempreVisivel) {
+                salas[key] = [];
+            }
+        });
+    } else {
+        salas['diarios'] = [];
+        salas['coordenacoes'] = [];
+    }
+    return salas;
+};
+
 const app = Vue.createApp({
     delimiters: ["[[", "]]"],
     components: {
@@ -102,9 +120,9 @@ const app = Vue.createApp({
             disciplinas: [],
             cursos: [],
             ambientes: [],
-            salasPorCategoria: {}, // Vai guardar { 'diarios': [...], 'praticas': [...] }
+            salasPorCategoria: getSalasIniciais(), // Inicializado dinamicamente via banco ou fallback
             activeTabKey: 'diarios',
-            loading: false,
+            loading: true,
         };
     },
     watch: {
@@ -225,6 +243,8 @@ const app = Vue.createApp({
         if (this.enableFilters) {
             this.loadFilters();
             this.filterCards();
+        } else {
+            this.loading = false;
         }
         this.sidebarContracted = this.isMobile();
     },
@@ -521,7 +541,7 @@ const app = Vue.createApp({
         handleFilterResponse(data) {
             const chavesDeFiltro = ['periodos', 'semestres', 'disciplinas', 'cursos', 'ambientes', 'modulos'];
 
-            this.salasPorCategoria = {};
+            this.salasPorCategoria = getSalasIniciais();
 
             Object.keys(data).forEach(key => {
                 if (chavesDeFiltro.includes(key)) {
